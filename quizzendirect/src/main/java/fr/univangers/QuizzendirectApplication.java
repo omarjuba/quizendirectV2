307d83ad86ab9a2cab7ea70b2f55efda858d2b12
etud user <etud@port-dell-precision-7530-20>
2021-09-27 21:55:32 +0200

    QUIZV2-6 : Par Aris Ondongi, ajout d'un champ mail pour les étudiants + class envoi mail ???FIN

--- quizzendirect/src/main/java/fr/univangers/QuizzendirectApplication.java
+++ quizzendirect/src/main/java/fr/univangers/QuizzendirectApplication.java
@@ -1,13 +1,16 @@
 package fr.univangers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class QuizzendirectApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(QuizzendirectApplication.class, args);
 	}
-
 }
