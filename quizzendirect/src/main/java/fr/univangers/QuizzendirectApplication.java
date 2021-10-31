d06f32f7efe08373e3c813e41ed5fe8813f5cf6f
Vi Quang <quanghua95@gmail.com>
2021-10-31 18:40:07 +0100

    nothing

--- quizzendirect/src/main/java/fr/univangers/QuizzendirectApplication.java
+++ quizzendirect/src/main/java/fr/univangers/QuizzendirectApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuizzendirectApplication {
-
-	
 	public static void main(String[] args) {
 		SpringApplication.run(QuizzendirectApplication.class, args);
 	}
